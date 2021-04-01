9c8113f5fe4d30ed1b75a5bfb4ce284d2e25b439
d-g-code <>
2021-04-01 19:20:40 +0200

    Decorator is a callable object that takes another function as an argument.

---
+++ python/decorator.py
@@ -0,0 +1,17 @@
+"""
+A decorator is a callable object that takes another function as an argument. The decorator processes the function
+to be decorated and then returns it or replaces it with another function or callable object
+"""
+
+def deco(func):
+    def inner():
+        print('running inner()')
+    return  inner
+
+@deco
+def target():
+    print('running target()')
+
+
+if __name__ == '__main__':
+    target()
