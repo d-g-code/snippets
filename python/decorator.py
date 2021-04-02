3cd86e12fc6f2434f71a2ab778577c6610e0051b
d-g-code <>
2021-04-02 14:37:32 +0200

    Correction pep8

--- python/decorator.py
+++ python/decorator.py
@@ -3,10 +3,12 @@ A decorator is a callable object that takes another function as an argument. The
 to be decorated and then returns it or replaces it with another function or callable object
 """
 
+
 def deco(func):
     def inner():
         print('running inner()')
-    return  inner
+    return inner
+
 
 @deco
 def target():
