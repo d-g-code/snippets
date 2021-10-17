ab66ae411bcfeb9484a2c061266e75e52a5c88f5
d-g-code <>
2021-10-17 23:47:53 +0200

    Ability to add words from a file

--- python/Repeating English/repeating_english_2.py
+++ python/Repeating English/repeating_english_2.py
@@ -1,13 +1,13 @@
 """
-Repeating English 2.0
+Repeating English 3.0
 - coloring answer
 - SQLite database
+- added new words from file
 """
 
-# https://realpython.com/python-sql-libraries/#creating-tables
 
 from termcolor import colored
-from functions_repeating_english_2 import create_connection, execute_read_query
+from functions_repeating_english_2 import create_connection, execute_read_query, execute_query
 
 
 connection = create_connection('/home/don/programming/snippets/python/Repeating English/repeating_english_2.db')
@@ -25,3 +25,15 @@ for row in range(len(repeating_words)):
         print(colored('BAD', 'red'))
         print(colored(repeating_words[row][1], 'green'))
         print(colored(user_answer, 'blue'), '\n')
+
+f = open('new_words', 'r')
+lines = f.readlines()
+for line in range(len(lines)):
+    row = lines[line]
+    cluster = row[0:-1].split()
+    new_word_eng = cluster[0]
+    new_word_pol = ' '.join(cluster[1:])
+    create_word = "INSERT INTO " \
+                  "repeating_word (word_eng, word_pol, amount_repeat) " \
+                  "VALUES ('{}', '{}', 0)".format(new_word_eng, new_word_pol)
+    execute_query(connection, create_word)
